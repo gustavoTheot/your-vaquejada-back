bee0c0bd90e45becdac56abdec94548c83cb4d48
gustavo theotonio <gustavotheotonio46@gmail.com>
2023-04-19 21:07:23 -0300

    new controllers

--- src/http/controller/manager/listManager.ts
+++ src/http/controller/manager/listManager.ts
@@ -1,7 +1,7 @@
 import {FastifyRequest, FastifyReply} from 'fastify'
 import { knex } from '../../../database'
 
-export async function updateManager(request: FastifyRequest, response: FastifyReply){
+export async function listManagers(request: FastifyRequest, response: FastifyReply){
     const data = await knex('manager').select('*')
 
     return response.status(201).send(data)
--- src/http/controller/manager/routes.ts
+++ src/http/controller/manager/routes.ts
@@ -1,6 +1,10 @@
 import {FastifyInstance} from 'fastify'
 import { createManager } from './createManager'
+import { listManagers } from './listManager'
+import { deleteManager } from './deleteManager'
 
 export function appRouter(app: FastifyInstance){
     app.post("/manager", createManager)
+    app.get("/manager", listManagers)
+    app.delete("manager/:id", deleteManager)
 }
