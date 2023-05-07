b9b58830fb5b8fd81f544e3aa8be8dfe3ca9987a
TaldoFernando <99918075+TaldoFernando@users.noreply.github.com>
2023-05-07 15:41:01 -0300

    Update createVaquejada.ts

--- src/useCases/createVaquejada.ts
+++ src/useCases/createVaquejada.ts
@@ -1,31 +1,31 @@
 import { knex } from "../database"
 
-interface CreateManagerUseCase{
+interface createManagerUseCase{
     title: string
     manager_id: string
 }
 
-export async function createVaquejadaUeCase({
-    title,
-    manager_id,
-   }: CreateManagerUseCase){
+export class CreateVaquejadaUeCase{
 
-    const dataManager = await knex('manager').select('*').where('id', manager_id).first()
+    async create({title, manager_id}: createManagerUseCase){
+        const dataManager = await knex('manager').select('*').where('id', manager_id).first()
 
-    if(!dataManager){
-        throw new Error('Manager does not exist')
+    
+        if(!dataManager){
+            throw new Error('Manager does not exist')
+        }
+
+        const {cowboy_number} = dataManager
+
+        if(cowboy_number <= 0){
+            throw new Error('Amount of cowboy exceeded')
+        }
+
+        await knex('manager').where({id: manager_id}).update({cowboy_number: cowboy_number-1})
+
+        await knex('vaquejada').insert({
+            title: title,
+            manager_id: manager_id
+        })
     }
-
-    const {cowboy_number} = dataManager
-
-    if(cowboy_number <= 0){
-        throw new Error('Amount of cowboy exceeded')
-    }
-
-    await knex('manager').where({id: manager_id}).update({cowboy_number: cowboy_number-1})
-
-    await knex('vaquejada').insert({
-        title: title,
-        manager_id: manager_id
-    })
 }
