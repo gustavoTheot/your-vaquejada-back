c1c995211320cdb515647825d28b79e0a6ec1e34
TaldoFernando <99918075+TaldoFernando@users.noreply.github.com>
2023-05-07 13:29:56 -0300

    Add files via upload

--- src/http/controller/vaqueiro/delete.ts
+++ src/http/controller/vaqueiro/delete.ts
@@ -1,10 +1,10 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import z from 'zod'
 import { knex } from "../../../database";
-import { criarvaqueiroUseCase } from "../../../useCases/criarvaqueiro";
+import { deletevaqueiroUseCase } from "../../../useCases/deletevaqueiro";
 
-export async function createCowboy(request: FastifyRequest, response: FastifyReply) {
-    const criarVaqueiroBodySchema = z.object({
+export async function deleteCowboy(request: FastifyRequest, response: FastifyReply) {
+    const deletevaqueiroBodySchema = z.object({
         name: z.string(),
         cpfv1: z.string().min(11).max(11),
         horse_name: z.string(),
@@ -12,19 +12,24 @@ export async function createCowboy(request: FastifyRequest, response: FastifyRep
         cpftread: z.string().min(11).max(11),
         tread_horse: z.string()
     })
+ 
 
-    const {name, cpfv1, horse_name, treadmill, tread_horse, cpftread} = criarVaqueiroBodySchema.parse(
+    
+
+
+
+    const {name, cpfv1, horse_name, treadmill, tread_horse, cpftread} = deletevaqueiroBodySchema.parse(
         request.body
     )
 
     try{
-        await criarvaqueiroUseCase({name, cpfv1, horse_name, treadmill, tread_horse, cpftread})
+        await deletevaqueiroUseCase({name, cpfv1, horse_name, treadmill, tread_horse, cpftread})
 
     }catch(error){
         if(error instanceof Error)
         return response.status(400).send({error: error.message})
     }
 
-    return response.status(201).send({message: 'Vaqueiro e Bate-esteira criados!'})
+    return response.status(201).send({message: 'Dados apagados com sucesso!'})
     
 }
