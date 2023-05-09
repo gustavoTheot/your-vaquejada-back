f27f6051510ffa36f5cd37f16469ebfb302c80d5
gustavo theotonio <gustavotheotonio46@gmail.com>
2023-05-08 21:47:23 -0300

    new commit

--- src/http/controller/vaqueiro/routes.ts
+++ src/http/controller/vaqueiro/routes.ts
@@ -2,5 +2,5 @@ import { FastifyInstance } from "fastify";
 import { deleteVaqueiro } from "./delete";
 
 export async function vaqueiroRoutes(app: FastifyInstance){
-    app.delete('/vaqueiro/id', deleteVaqueiro)
+    app.delete('/vaqueiro/:id', deleteVaqueiro)
 }
